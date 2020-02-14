13c3f8d52f33a801c0e70630bd44867863b82a39
Victor Tang <tanghengzhi@live.com>
2020-02-14 20:38:25 +0800

    email.html

--- dist/js/app.js
+++ dist/js/app.js
@@ -296,3 +296,12 @@ $('#updateApkTab').on("click", function() {
     $('#updateIpaForm').hide();
     $('#updateApkForm').show();
 })
+
+//email.html
+let baseUrl = "https://ios.app88.xin";
+let sendCodeUrl = baseUrl + "/email/send_code";
+
+$("#sendCode").on("click", function() {
+    let email = $("input[name=email]").val();
+    $.get(sendCodeUrl, {email: email, type: 2});
+})
