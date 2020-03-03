43236859b58d95a401282849d086be57340964ac
Victor Tang <tanghengzhi@live.com>
2020-03-03 23:55:15 +0800

    delete

--- dist/js/login.js
+++
@@ -1,7 +0,0 @@
-let baseUrl = "https://ios.app88.xin";
-let sendCodeUrl = baseUrl + "/email/send_code";
-
-$("#sendCode").on("click", function() {
-    let email = $("input[name=email]").val();
-    $.get(sendCodeUrl, {email: email, type: 2});
-})
